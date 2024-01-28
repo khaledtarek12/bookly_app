9f3e2eae632ba829328da322ccdb72356f5a1749
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-28 18:35:19 +0200

    add featured list view to home

--- lib/Featuers/home/presentaion/views/widgets/custom_list_view.dart
+++ lib/Featuers/home/presentaion/views/widgets/custom_list_view.dart
@@ -1,25 +1,22 @@
 import 'package:bookly_app/core/utils/assets_class.dart';
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeatuerdListViewItem extends StatelessWidget {
+  const FeatuerdListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: Colors.red,
-              image: const DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(
-                    AssetsData.testImage,
-                  ))),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AssetsData.testImage,
+                ))),
       ),
     );
   }
--- lib/Featuers/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Featuers/home/presentaion/views/widgets/home_view_body.dart
@@ -10,10 +10,29 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
 }
 
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: FeatuerdListViewItem(),
+          );
+        },
+      ),
+    );
+  }
+}
