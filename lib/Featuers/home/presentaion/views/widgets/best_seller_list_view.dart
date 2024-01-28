35c885a521d2e1f10be38358e28ee442e4ffa72b
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-28 20:47:46 +0200

    beset seller done

---
+++ lib/Featuers/home/presentaion/views/widgets/best_seller_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'besst_seller_item_view.dart';
+
+class BesetSellerListViewBuilder extends StatelessWidget {
+  const BesetSellerListViewBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.only(bottom: 20, right: 10),
+          child: BesstSellerListViewItem(),
+        );
+      },
+    );
+  }
+}
--- lib/Featuers/home/presentaion/views/widgets/home_view_body.dart
+++ lib/Featuers/home/presentaion/views/widgets/home_view_body.dart
@@ -3,26 +3,32 @@ import 'package:bookly_app/Featuers/home/presentaion/views/widgets/feautred_list
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
-import 'besst_seller_item_view.dart';
+import 'best_seller_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-        SizedBox(height: 50),
-        Padding(
-            padding: EdgeInsets.only(left: 24, right: 24, bottom: 20),
-            child: Text('Besst Seller', style: Styles.styleText18)),
-        BesstSellerListViewItem(),
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAppBar(),
+              FeaturedBooksListView(),
+              SizedBox(height: 20),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  child: Text('Besst Seller', style: Styles.styleText18)),
+            ],
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: BesetSellerListViewBuilder(),
+        )
       ],
     );
   }
 }
-
-
