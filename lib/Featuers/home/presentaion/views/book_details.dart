89d91dca132a1a95ab698e3256e7f5086da068c4
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-28 20:57:04 +0200

    book details view

---
+++ lib/Featuers/home/presentaion/views/book_details.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Featuers/home/presentaion/views/widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetails extends StatelessWidget {
+  const BookDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
