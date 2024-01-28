a1af3e431159015dbf446a47ce8c61b8382542d5
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-28 19:36:52 +0200

    styles part2

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,26 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
   static const styleText18 = TextStyle(
     fontWeight: FontWeight.w600,
     fontSize: 18,
+  );
+  static const styleText20 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 20,
+    fontFamily: kgtSectraFine,
+  );
+  static const styleText14 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 14,
+  );
+  static const styleText16 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+    static const styleText30 = TextStyle(
+    fontWeight: FontWeight.normal,
+    fontSize: 30,
   );
 }
