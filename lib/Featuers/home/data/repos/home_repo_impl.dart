4bf5d1fc33d5ebac6751f706c336ae062ac1e452
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-29 18:04:32 +0200

    raper around api

---
+++ lib/Featuers/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Featuers/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Featuers/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImplementaion extends HomeRepo  {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/core/utils/api_services.dart
@@ -0,0 +1,12 @@
+import 'package:dio/dio.dart';
+
+class ApiServices {
+  final _baseUrl = 'https://www.googleapis.com/books/v1/';
+  final Dio dio = Dio();
+
+  Future<Map<String, dynamic>> get({required String endPoints}) async {
+    var response = await dio.get('$_baseUrl$endPoints');
+
+    return response.data;
+  }
+}
