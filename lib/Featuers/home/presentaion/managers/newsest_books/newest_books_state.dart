194ff76eea90bdaa696e83eeaa5a2b0a7866a23f
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-29 23:29:40 +0200

    newset cuboit done

--- lib/Featuers/home/presentaion/managers/newsest_books/newest_books_state.dart
+++ lib/Featuers/home/presentaion/managers/newsest_books/newest_books_state.dart
@@ -8,16 +8,16 @@ abstract class NewestBooksState extends Equatable {
 }
 
 class NewestBooksInitial extends NewestBooksState {}
-class FeaturedBoxLoading extends NewestBooksState {}
+class NewestBooksLoading extends NewestBooksState {}
 
-class FeaturedBoxSuccess extends NewestBooksState {
+class NewestBooksSuccess extends NewestBooksState {
   final List<BookModel> books;
 
-  const FeaturedBoxSuccess({required this.books});
+  const NewestBooksSuccess({required this.books});
 }
 
-class FeaturedBoxFailure extends NewestBooksState {
+class NewestBooksFailure extends NewestBooksState {
   final String errorMessage;
 
-  const FeaturedBoxFailure({required this.errorMessage});
+  const NewestBooksFailure({required this.errorMessage});
 }
