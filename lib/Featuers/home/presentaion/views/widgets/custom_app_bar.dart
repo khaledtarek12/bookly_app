d620bcaa8f0245f73d6b862d7fdbe28a5d60edb4
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-29 03:46:42 +0200

    create custom text filed

--- lib/Featuers/home/presentaion/views/widgets/custom_app_bar.dart
+++ lib/Featuers/home/presentaion/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets_class.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({
@@ -10,7 +12,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 40, bottom: 25 , left: 24 , right: 24),
+      padding: const EdgeInsets.only(top: 40, bottom: 25, left: 24, right: 24),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -19,7 +21,9 @@ class CustomAppBar extends StatelessWidget {
             height: 18,
           ),
           IconButton(
-              onPressed: () {},
+              onPressed: () {
+                GoRouter.of(context).push(AppRouter.ksearchView);
+              },
               icon: const Icon(
                 FontAwesomeIcons.magnifyingGlass,
                 size: 24,
