9f6bad135950f32e6134e1d07064b721003f3e45
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-29 23:21:27 +0200

    create featured  box emit

--- lib/Featuers/home/presentaion/managers/featured_box/featured_box_cubit.dart
+++ lib/Featuers/home/presentaion/managers/featured_box/featured_box_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bookly_app/Featuers/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Featuers/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'featured_box_state.dart';
 
 class FeaturedBoxCubit extends Cubit<FeaturedBoxState> {
-  FeaturedBoxCubit() : super(FeaturedBoxInitial());
+  FeaturedBoxCubit(this.homeRepo) : super(FeaturedBoxInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeauturdBooks() async {
+    emit(FeaturedBoxLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold((failure) {
+      emit(FeaturedBoxFailure(errorMessage: failure.errorMessage));
+    }, (books) {
+      emit(FeaturedBoxSuccess(books: books));
+    });
+  }
 }
