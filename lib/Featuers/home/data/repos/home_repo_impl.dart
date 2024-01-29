d8f7bc1042e9c94129a9474ae72f45263f1ca421
khaledtarek12 <tareqkhalid092@gmail.com>
2024-01-29 18:59:42 +0200

    finish home repo

--- lib/Featuers/home/data/repos/home_repo_impl.dart
+++ lib/Featuers/home/data/repos/home_repo_impl.dart
@@ -32,7 +32,22 @@ class HomeRepoImplementaion extends HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoints: 'volumes?q=subject:programming&Filtering=free-ebooks');
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errorMessage: e.toString()));
+    }
   }
 }
